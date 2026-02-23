1eb50fc044ce4c4040a8172411b8c2e97277c828
clarencegabrielong <clarencegabriel21@gmail.com>
2026-02-23 15:44:12 -0500

    added font, added scoring, added another while loop to optimize main

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ from logger import log_event
 from player import Player
 from shot import Shot
 
-def game_over(screen):
-    font = pygame.font.Font(None, 74)
+def game_over(screen, final_score):
+    font = pygame.font.Font(FONT_PATH, 60)
     text = font.render("GAME OVER", True, (255, 255, 255))
-    text_rect = text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
-    
-    prompt_font = pygame.font.Font(None, 36)
+    text_rect = text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 65))
+
+    score_font = pygame.font.Font(FONT_PATH, 40)
+    score_text = score_font.render(f"Score: {final_score}", True, (255, 255, 255))
+    score_rect = score_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 + 5))    
+
+    prompt_font = pygame.font.Font(FONT_PATH, 28)
     prompt = prompt_font.render("Press R to Restart or Q to Quit", True, (255, 255, 255))
-    prompt_rect = prompt.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 + 50))
+    prompt_rect = prompt.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 + 60))
     
     while True:
         for event in pygame.event.get():
@@ -29,75 +33,94 @@ def game_over(screen):
                     return False 
         
         screen.fill((0, 0, 0))
-        screen.blit(text, text_rect)
+        screen.blit(text, text_rect) # name better?
+        screen.blit(score_text, score_rect)
         screen.blit(prompt, prompt_rect)
         pygame.display.flip()
 
+score = 0
+
+def destroyed_asteroid(asteroid):
+
+    if asteroid.radius == ASTEROID_MIN_RADIUS:
+        return 100
+    elif asteroid.radius == ASTEROID_MIN_RADIUS * 2:
+        return 50 
+    else:
+        return 20
+
+
+
 def main():
 
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
+
+    score_font = pygame.font.Font(FONT_PATH, 24)
 
     pygame.display.set_caption("Asteroids")
     icon = pygame.image.load("assets/icon.png")
     pygame.display.set_icon(icon)
 
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group()
-    shots = pygame.sprite.Group()
-
-    Player.containers = (updatable, drawable)
-    Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
-    Shot.containers = (updatable, drawable, shots)
-
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    asteroidfield = AsteroidField()
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED)
 
     clock = pygame.time.Clock()
 
-    dt = 0
-
-
-
     while True:
-        log_state()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-            
-        updatable.update(dt)
+        score = 0
 
-        screen.fill("black")
+        updatable = pygame.sprite.Group()
+        drawable = pygame.sprite.Group()
+        asteroids = pygame.sprite.Group()
+        shots = pygame.sprite.Group()
 
-        for asteroid in asteroids:
-            if player.collides_with(asteroid):
-                if game_over(screen):
-                    return main()
-                else:
+        Player.containers = (updatable, drawable)
+        Asteroid.containers = (asteroids, updatable, drawable)
+        AsteroidField.containers = (updatable,)
+        Shot.containers = (updatable, drawable, shots)
+
+        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        asteroidfield = AsteroidField()
+
+        dt = 0
+
+        restart = False
+
+        while not restart:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     return
-            for shot in shots:
-                if asteroid.collides_with(shot):
-                    log_event("asteroid_shot")
-                    asteroid.split()
-                    shot.kill()
+                
+            updatable.update(dt)
+
+            screen.fill("black")
+
+            for asteroid in asteroids:
+                if player.collides_with(asteroid):
+                    if game_over(screen, score):
+                        restart = True
+                    else:
+                        return
+                    
+                for shot in shots:
+                    if asteroid.collides_with(shot):
+                        score += destroyed_asteroid(asteroid)
+                        asteroid.split()
+                        shot.kill()
+                
+
+            for sprite in drawable:
+                sprite.draw(screen)
             
+            score_text = score_font.render(f"Score:{score}", True, (255, 255, 255))
+            screen.blit(score_text, (10, 10))
 
-        for sprite in drawable:
-            sprite.draw(screen)
-        
-
-        pygame.display.flip()
-        dt = clock.tick(60)/1000
+            pygame.display.flip()
+            dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
-    main()
-    pygame.quit()
+    try:
+        main()
+    finally:
+        pygame.quit()
